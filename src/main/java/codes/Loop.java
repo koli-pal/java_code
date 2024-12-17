24b0735215ac76714613efe9864810695fd4be18
Koli-pal <palkoli581@gmail.com>
2024-12-18 00:54:10 +0600

    second commit

---
+++ src/main/java/codes/Loop.java
@@ -0,0 +1,12 @@
+package codes;
+
+public class Loop {
+    public static  void main (String[]args){
+        int countdown = 3;
+        while (countdown>0){
+            System.out.println(countdown);
+            countdown--;
+        }
+        System.out.println("Happy New Year!");
+    }
+}
